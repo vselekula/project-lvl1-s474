36d8a552dbcdd613a06aeb5cf6d0d59cb48a0de2
Vjacheslav Petrov <slavamarsmgn@gmail.com>
2019-04-15 08:49:58 +0300

    ...

---
+++ src/bin/brain-games.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+
+console.log('Hi! Choose a game from the list and just enter the name of the game!');
+console.log('brain-calc');
+console.log('brain-even');
+console.log('brain-gcd');
+console.log('brain-prime');
+console.log('brain-progression');
